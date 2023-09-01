700e16377baaa824c1f4bd54d2db2bc323d9ed63
MartinGao <martingaoorglz@gmail.com>
2023-09-01 21:25:33 +1000

    Thesis origin commit

---
+++ aRMT/Components/PageCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Dimensions, Text } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const PageCard = (props: { title: any; iconName: any; }) => { 
+    // get phone screen width and height
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
+    // get card width and height
+    const marginWidth = screenWidth * 0.05;
+    const marginHeight = screenHeight * 0.05;
+    const cardWidth = screenWidth * 0.4;
+    const cardHeight = screenHeight * 0.25;
+    // get arguments
+    const title = props.title;
+    const iconName = props.iconName;
+    
+    return (
+        <View style={
+            {
+                backgroundColor: '#CCE8E9',
+                borderRadius: 16,
+                width: cardWidth,
+                height: cardHeight,
+                marginHorizontal: marginWidth,
+                marginTop: marginHeight,
+            }
+        }>
+            <MaterialIcons name={iconName} style={{
+                color: 'black',
+                fontSize: 30,
+                top: '20%',
+                left: '10%'
+            }}></MaterialIcons>
+            <Text style={{
+                color: 'black',
+                fontSize: 20,
+                top: '50%',
+                left: '10%'
+            }}>{title}</Text>
+        </View>
+    )
+}
+
+export default PageCard;
---
+++ aRMT/Pages/Home.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PageCard from "../Components/PageCard";
+import { View, Dimensions, Text } from "react-native"
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const Home = () => {
+    // get phone screen size
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
+
+    /**
+     * 
+     */
+    const openPage = () => {
+        
+    };
+    
+    return (
+        <View id="Page" style={{ 
+            position: "relative",
+            height: screenHeight,
+            width: screenWidth,
+
+        }}>
+            <MaterialIcons name="notifications" style={{
+                color: 'black',
+                fontSize: 25,
+                top: '6%',
+                left: '80%'
+            }}></MaterialIcons>
+            <MaterialIcons name="more-vert" style={{
+                color: 'black',
+                fontSize: 25,
+                top: '3%',
+                left: '90%'
+            }}></MaterialIcons>
+            <Text style={{
+                textAlign: "center",
+                top: '10%',
+                fontSize: 30,
+                color: 'black'
+            }}>
+                Welcome    
+            </Text>
+            <View id="cardContainer" style={{
+                position: "absolute",
+                height: '65%',
+                width: '100%',
+                top: '30%',
+                bottom: '5%',
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap"
+                }}>
+                <PageCard iconName={"today"} title={"Today"}></PageCard>
+                <PageCard iconName={"pending-actions"} title={"Scheduled"}></PageCard>
+                <PageCard iconName={"done"} title={"Finished"}></PageCard>
+                <PageCard iconName={"assignment-late"} title={"Overdue"}></PageCard>
+            </View>
+        </View>
+    )
+}
+
+export default Home;
